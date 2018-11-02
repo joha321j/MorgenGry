446ba2bc8b5f80bf24fae44851af6b5f433b4b4f
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2018-11-02 09:05:07 +0100

    Solve Ex20-Persistable.

---
+++ MorgenGry/IPersistable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorgenGry
+{
+    public interface IPersistable
+    {
+        void Save();
+
+        void Save(string fileName);
+
+        void Load();
+
+        void Load(string fileName);
+
+    }
+}
--- MorgenGry/ValuableRepository.cs
+++ MorgenGry/ValuableRepository.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MorgenGry
 {
-    public class ValuableRepository
+    public class ValuableRepository : IPersistable
     {
         private List<IValuable> valuableList = new List<IValuable>();
 
@@ -46,6 +47,99 @@ namespace MorgenGry
         public int Count()
         {
             return valuableList.Count();
+        }
+
+        public void Save()
+        {
+            SaveFile();
+        }
+
+        public void Save(string fileName)
+        {
+            SaveFile(fileName);
+        }
+
+        private void SaveFile(string fileName = "C:\\Users\\Johannes\\Documents\\Visual Studio 2017\\Projects\\MorgenGry\\MorgenGry\\bin\\DebugValuableRepository.txt")
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(@fileName))
+                {
+                    foreach (IValuable valuable in valuableList)
+                    {
+                        if (valuable is Course course)
+                        {
+                            writer.WriteLine("KURSUS;{0};{1};{2}", course.Name, course.CourseHourValue, course.DurationInMinutes);
+                        }
+
+                        if (valuable is Book book)
+                        {
+                            writer.WriteLine("BOG;{0};{1};{2}", book.ItemId, book.Title, book.Price);
+                        }
+
+                        if (valuable is Amulet amulet)
+                        {
+                            writer.WriteLine("AMULET;{0};{1};{2}", amulet.ItemId, amulet.Design, amulet.Quality);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void Load()
+        {
+            LoadFile();
+        }
+
+        public void Load(string fileName)
+        {
+            LoadFile(fileName);
+        }
+
+        private void LoadFile(string fileName = "C:\\Users\\Johannes\\Documents\\Visual Studio 2017\\Projects\\MorgenGry\\MorgenGry\\bin\\DebugValuableRepository.txt")
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(@fileName))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] elements = line.Split(';');
+
+                        if (Equals(elements[0], "BOG"))
+                        {
+                            Book book = new Book(elements[1], elements[2], double.Parse(elements[3]));
+                            AddValuable(book);
+                        }
+
+                        if (Equals(elements[0], "AMULET"))
+                        {
+                            Level level = (Level) Enum.Parse(typeof(Level), elements[3]);
+                            Amulet amulet = new Amulet(elements[1], level, elements[2]);
+                            AddValuable(amulet);
+                        }
+
+                        if (Equals(elements[0], "KURSUS"))
+                        {
+                            Course course = new Course(elements[1], int.Parse(elements[3]), double.Parse(elements[2]));
+                            AddValuable(course);
+                        }
+                    }
+                    
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
